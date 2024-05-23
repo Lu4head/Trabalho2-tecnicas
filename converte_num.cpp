c0b9931b0f0fce9c19143fe6b4b466c947aa33c8
Luan <luan.emanuelriar@gmail.com>
2024-05-22 21:10:08 -0300

    ahhh

--- converte_num.cpp
+++ converte_num.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main() {
     // Definindo o intervalo de números e o número de caracteres
-    int min = 0;
-    int max = 255;
+    int min = 50;
+    int max = 100;
     const int numCaracteres = 67;
     char caracteres[numCaracteres] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`";
     double variacao = (1 + max - min) / numCaracteres;
-    
     for(int i = min ; i <= max ; ++i){
         int indice = (i - min) / (variacao + 1);
         if(indice >= numCaracteres) {
