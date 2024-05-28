c13cc743d2ffdc25f571ece144a56788fd8e81af
Luan <luan.emanuelriar@gmail.com>
2024-05-28 13:34:25 -0300

    idêntico ao do professor :)

--- testes.cpp
+++ testes.cpp
@@ -5,45 +5,48 @@
 
 using namespace std;
 
-char converte_registro(char registro) {
+char converte_registro(unsigned char registro) {
 
     const char caracteres[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`.";
+    double num_caracteres = sizeof(caracteres); // Número de elementos na lista de caracteres
+    double tam_intervalos = 256 / num_caracteres; // Tamanho de cada intervalo 
+    double intervalo = 256 / tam_intervalos; // Número de intervalos em que será dividida a lista de caracteres
 
-
-    const int num_intervalos = 4;
-    const int intervalo = 256 / num_intervalos;
-
-    
-    int indice = registro / intervalo;
+    int indice = registro / intervalo; // Indice da lista de caracteres a ser utilizado com base no valor do registro
 
     // Certificar-se de que o índice está dentro dos limites
     if (indice < 0) indice = 0;
-    else if (indice >= num_intervalos) indice = num_intervalos - 1;
-
+    else if (indice >= num_caracteres - 1) indice = num_caracteres - 1;
+    
     return caracteres[indice];
 }
 
-int main(){
-    setlocale(LC_ALL,"Portuguese");
-    const char nome_arquivo_input[] = "img2.foo5" ;
-    ifstream input_file("./Exemplos/img2.foo", ios::binary);
-    ofstream output_file(nome_arquivo_input);
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Portuguese");
 
-    if(input_file.fail()){
-        cout << "Erro ao abrir o arquivo " << nome_arquivo_input << endl;
+    if (argc < 3) { // Verifica se todos os parametros necessários foram passados
+        cout << "Uso: " << argv[0] << " <arquivo_de_entrada> <arquivo_de_saida>" << endl;
         return 1;
     }
 
-    char registro;
+    ifstream input_file(argv[1], ios::binary); // argv[1] é o nome do arquivo de entrada	
+    ofstream output_file(argv[2]); // argv[2] é o nome do arquivo de saída
+
+
+    if(input_file.fail()){
+        cout << "Erro ao abrir o arquivo " << argv[1] << endl;
+        return 1;
+    }
+
+    unsigned char registro;
     int altura , largura;
     input_file >> largura >> altura;
     output_file << largura << " " << altura << endl;
 
     for(int i = 0; i < altura ; ++i){
         for(int j = 0; j < largura ; ++j){
-            input_file.read(&registro, 1);
-            char registro_convertido = converte_registro(registro);
-            output_file << registro_convertido;
+            input_file.read(reinterpret_cast<char*>(&registro), 1);
+            output_file << converte_registro(registro);
         }
         output_file << '\n';
     }
--- trabalho2-tec.cpp
+++ trabalho2-tec.cpp
@@ -4,71 +4,45 @@
 
 using namespace std;
 
-int converte_registro(char registro){
-    int registro_int = (char) registro;
+unsigned char converte_registro(unsigned char registro){
+    int min = 0 , max = 255;
+    const unsigned char caracteres[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`.";
+    int numCaracteres = sizeof(caracteres) - 1;
+    double variacao =  (max - min + 1) / (numCaracteres);
 
-    // int min = 0;
-    // int max = 255;
-    // char caracteres[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`";
-    // int numCaracteres = sizeof(caracteres);
-    // double variacao = static_cast<double>(max - min + 1) / (numCaracteres - 1);
-
-    // int indice = static_cast<int>((registro_int - min) / variacao);
-        
-    // char registro_convertido = caracteres[indice];
-
-    //return registro_convertido;
-
-    return registro_int;
+    int indice = (registro - min) / variacao;
+    if (indice < 0) indice = 0;
+    else if (indice >= numCaracteres) indice = numCaracteres - 1;    
+    return caracteres[indice];
 }
+
 
 int main(){
     setlocale(LC_ALL,"Portuguese");
 
-    ifstream input_file("./Exemplos/img0.foo", ios::binary);
-    ofstream output_file("img0.foo2");
+    const char nome_arquivo_input[] = "img0.foo" ;
+    const char nome_arquivo_output[] = "img0.foo2" ;
+
+    ifstream input_file(nome_arquivo_input, ios::binary);
+    ofstream output_file(nome_arquivo_output);
 
     if(input_file.fail()){
-        cout << "Erro ao abrir o arquivo de entrada" << endl;
+        cout << "Erro ao abrir o arquivo " << nome_arquivo_input << endl;
         return 1;
     }
-    if(output_file.fail()){
-        cout << "Erro ao abrir o arquivo de saída" << endl;
-        return 1;
+    unsigned char registro;
+    int altura , largura;
+    input_file >> largura >> altura;
+    output_file << largura << " " << altura << endl;
+
+    for(int i = 0; i < altura ; ++i){
+        for(int j = 0; j < largura ; ++j){
+            input_file.read(reinterpret_cast<char*>(&registro), 1);
+            output_file << converte_registro(registro);
+        }
+        output_file << '\n';
     }
 
-    char registro;
-    input_file.seekg(0, ios::end);
-    int tamanho_arquivo = input_file.tellg();
-    input_file.seekg(0, ios::beg);
-    int* dados_convertidos = new int[tamanho_arquivo];
-    int tamanho_primeira_linha = 0;
-    bool escrever_como_char = true; // Variável para controlar o modo de escrita
-
-    for(int indice = 0; indice < tamanho_arquivo; ++indice){
-        input_file.read(&registro, sizeof(registro));
-        
-        if (escrever_como_char) {
-            output_file << registro; // Escreve como char
-            tamanho_primeira_linha++;
-        } else {
-            char registro_convertido = converte_registro(registro);
-            dados_convertidos[indice - tamanho_primeira_linha] = registro_convertido;
-        }
-
-        if (registro == '\n') {
-            escrever_como_char = false; // Altera o modo de escrita para int
-        }
-
-    }
-
-    if (!escrever_como_char) {
-        for(int i = 0; i < tamanho_arquivo - tamanho_primeira_linha; ++i){
-            output_file << dados_convertidos[i] << " "; // Escreve como int
-        }
-    }
-
-    delete[] dados_convertidos;
     input_file.close();
     output_file.close();   
 
